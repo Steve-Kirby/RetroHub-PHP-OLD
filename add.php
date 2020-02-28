d916abfb27e741b92905911e137492ceb0f1f5ba
Steve-Kirby <59378715+Steve-Kirby@users.noreply.github.com>
2020-02-28 17:07:14 +0000

    Removed quotation marks causing item not to add

--- add.php
+++ add.php
@@ -3,7 +3,7 @@ if(isset($_GET['add'])){
   if(!isset($_SESSION['user'])){
     header("Location: login.php");
   } else {
-    $statement = $conn->prepare("INSERT INTO `cart`(`userID`, `itemID`) VALUES ('?','?')");
+    $statement = $conn->prepare("INSERT INTO `cart`(`userID`, `itemID`) VALUES (?,?)");
     $statement->bind_param("ii",$_SESSION['user'], $_GET['add']);
     $statement->execute();
   }
